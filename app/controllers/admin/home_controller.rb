900e23bd7b46d7a3c4b1f096944d9b605cb008e6
leandroalvesmachado <leandroalvesmachado@gmail.com>
2023-09-29 15:11:46 -0300

    adicionando projeto

---
+++ app/controllers/admin/home_controller.rb
@@ -0,0 +1,31 @@
+class Admin::HomeController < ApplicationController
+  layout 'admin'
+  
+  def index
+    # Your index action logic here
+  end
+
+  def show
+    # Your show action logic here
+  end
+
+  def new
+    # Your new action logic here
+  end
+
+  def create
+    # Your create action logic here
+  end
+
+  def edit
+    # Your edit action logic here
+  end
+
+  def update
+    # Your update action logic here
+  end
+
+  def destroy
+    # Your destroy action logic here
+  end
+end
---
+++ app/javascript/admin.js
@@ -0,0 +1,9 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+import "./jquery"
+
+import './admin/bootstrap.bundle.min.js'
+// import './admin/jquery.overlayScrollbars.min.js'
+import './admin/adminlte.min.js'
+import './admin/adminlte_demo.js'
